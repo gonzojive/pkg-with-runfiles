9f391d1cf6edc46eb828281bdd80e546987b6942
Red Daly <reddaly@google.com>
2024-10-06 19:16:33 -0700

    implement test program

---
+++ tests/bzlmod-example-intermod/rooty/go.mod
@@ -0,0 +1,5 @@
+module example.com/rooty
+
+go 1.23.1
+
+require github.com/bazelbuild/rules_go v0.50.1
---
+++ tests/bzlmod-example-intermod/rooty/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bazelbuild/rules_go/go/runfiles"
+)
+
+func main() {
+	rfiles, err := runfiles.New()
+	if err != nil {
+		fmt.Printf("error loading runfiles: %v\n", err)
+		os.Exit(1)
+	}
+	loc, err := rfiles.Rlocation("data1_from_rooty/message.txt")
+	if err != nil {
+		fmt.Printf("error loading runfile message.txt: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("hello, world %s\n", loc)
+}
